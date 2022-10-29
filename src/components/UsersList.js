39910159fbec7c7907e4770cd7c497110a6d1ef9
Davidlloyd8 <extendavidba4@gmail.com>
2022-10-29 02:07:33 +0100

    Ammendment on fetching the API

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -16,17 +16,19 @@ function UsersList() {
   }
   const [page, setPage] = useState(1);
   const { loading, error, data } = useFetch(
-    `https://randomuser.me/api/?page=${page}&results=6&seed=abc`
+    `https://randomuser.me/api/?results=40&seed=abc`
+    //https://randomuser.me/api/?page=${page}&results=6&seed=abc
   );
   const PER_PAGE = 6;
   const total = data?.results?.length;
-  const pages = 10;
+  //   const pages = 10;
+  const pages = Math.ceil(total / PER_PAGE);
   const skip = page * PER_PAGE - PER_PAGE;
   if (loading) {
     return <>Loading...</>;
   }
 
-  if (!loading && error) {
+  if ((!loading && error) || !data) {
     return <>Error</>;
   }
 
@@ -40,37 +42,40 @@ function UsersList() {
       >
         <h1>LIST OF USERS</h1>
         <div class="box">
-          {data?.results.map((each, index) => {
-            const name = `${each.name.title} ${each.name.first} ${each.name.last}`;
-            const image = `${each.picture.large}`;
-            const nationality = `${each.location.country}`;
-            return (
-              <div class="card">
-                <div class="imgBx">
-                  <img src={image} alt="images" />
+          {/* // (page -1) * PER_PAGE, page * PER_PAGE    skip, skip + PER_PAGE*/}
+          {data?.results
+            .slice((page - 1) * PER_PAGE, page * PER_PAGE)
+            .map((each, index) => {
+              const name = `${each.name.title} ${each.name.first} ${each.name.last}`;
+              const image = `${each.picture.large}`;
+              const nationality = `${each.location.country}`;
+              return (
+                <div class="card">
+                  <div class="imgBx">
+                    <img src={image} alt="images" />
+                  </div>
+                  <div class="details">
+                    <h2 key={name.toLowerCase().replaceAll(" ", "")}>
+                      {`${index + 1}.${name}`}
+                      <br />
+                      <span>{nationality}</span>
+                    </h2>
+                  </div>
                 </div>
-                <div class="details">
-                  <h2 key={name.toLowerCase().replaceAll(" ", "")}>
-                    {`${index + 1}.${name}`}
-                    <br />
-                    <span>{nationality}</span>
-                  </h2>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
 
         <div className="pagination-container">
           <ul className="pagination">
             {
-              <li
+              <button
                 className="prev"
                 disabled={page <= 1}
                 onClick={() => setPage((prev) => prev - 1)}
               >
                 Prev
-              </li>
+              </button>
             }
             {Array.from({ length: pages }, (_, index) => index + 1).map(
               (each) => (
@@ -78,13 +83,13 @@ function UsersList() {
               )
             )}
             {
-              <li
+              <button
                 className="next"
                 disabled={page >= pages}
                 onClick={() => setPage((prev) => prev + 1)}
               >
                 Next
-              </li>
+              </button>
             }
           </ul>
         </div>
