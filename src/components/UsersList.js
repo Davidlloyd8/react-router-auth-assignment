85adf956fe4bf894338eceba233a4d2e15bc8d1e
Davidlloyd8 <extendavidba4@gmail.com>
2022-10-31 16:15:45 +0100

    refactorize the code for error boundary

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,23 +1,12 @@
 import { useState } from "react";
 import useFetch from "../useFetch";
-import { ErrorBoundary } from "react-error-boundary";
 import "../pagination.css";
 import "../css/users.css";
 
 function UsersList() {
-  function ErrorFallback({ error, resetErrorBoundary }) {
-    return (
-      <div role="alert">
-        <p>Something went wrong:</p>
-        <pre style={{ color: "red" }}>{error.message}</pre>
-        <button onClick={resetErrorBoundary}>Try again</button>
-      </div>
-    );
-  }
   const [page, setPage] = useState(1);
   const { loading, error, data } = useFetch(
     `https://randomuser.me/api/?results=40&seed=abc`
-    //https://randomuser.me/api/?page=${page}&results=6&seed=abc
   );
   const PER_PAGE = 6;
   const total = data?.results?.length;
@@ -34,66 +23,59 @@ function UsersList() {
 
   return (
     <>
-      <ErrorBoundary
-        FallbackComponent={ErrorFallback}
-        onReset={() => setPage(1)}
-        // reset the state of your app so the error doesn't happen again
-        resetKeys={[page]}
-      >
-        <h1>LIST OF USERS</h1>
-        <div className="box">
-          {/* // (page -1) * PER_PAGE, page * PER_PAGE    skip, skip + PER_PAGE*/}
-          {data?.results
-            .slice((page - 1) * PER_PAGE, page * PER_PAGE)
-            .map((each, index) => {
-              const name = `${each.name.title} ${each.name.first} ${each.name.last}`;
-              const image = `${each.picture.large}`;
-              const nationality = `${each.location.country}`;
-              return (
-                <div className="card">
-                  <div className="imgBx">
-                    <img src={image} alt="images" />
-                  </div>
-                  <div className="details">
-                    <h2 key={name.toLowerCase().replaceAll(" ", "")}>
-                      {`${index + 1}.${name}`}
-                      <br />
-                      <span>{nationality}</span>
-                    </h2>
-                  </div>
+      <h1>LIST OF USERS</h1>
+      <div className="box">
+        {/* // (page -1) * PER_PAGE, page * PER_PAGE    skip, skip + PER_PAGE*/}
+        {data?.results
+          .slice((page - 1) * PER_PAGE, page * PER_PAGE)
+          .map((each, index) => {
+            const name = `${each.name.title} ${each.name.first} ${each.name.last}`;
+            const image = `${each.picture.large}`;
+            const nationality = `${each.location.country}`;
+            return (
+              <div className="card">
+                <div className="imgBx">
+                  <img src={image} alt="images" />
                 </div>
-              );
-            })}
-        </div>
+                <div className="details">
+                  <h2 key={name.toLowerCase().replaceAll(" ", "")}>
+                    {`${index + 1}.${name}`}
+                    <br />
+                    <span>{nationality}</span>
+                  </h2>
+                </div>
+              </div>
+            );
+          })}
+      </div>
 
-        <div className="pagination-container">
-          <ul className="pagination">
-            {
-              <button
-                className="prev"
-                disabled={page <= 1}
-                onClick={() => setPage((prev) => prev - 1)}
-              >
-                Prev
-              </button>
-            }
-            {Array.from({ length: pages }, (_, index) => index + 1).map(
-              (each) => (
-                <li onClick={() => setPage(each)}>{each}</li>
-              )
-            )}
-            {
-              <button
-                className="next"
-                disabled={page >= pages}
-                onClick={() => setPage((prev) => prev + 1)}
-              >
-                Next
-              </button>
-            }
-          </ul>
-        </div>
-      </ErrorBoundary>
+      <div className="pagination-container">
+        <ul className="pagination">
+          {
+            <button
+              className="prev"
+              disabled={page <= 1}
+              onClick={() => setPage((prev) => prev - 1)}
+            >
+              Prev
+            </button>
+          }
+          {Array.from({ length: pages }, (_, index) => index + 1).map(
+            (each) => (
+              <li onClick={() => setPage(each)}>{each}</li>
+            )
+          )}
+          {
+            <button
+              className="next"
+              disabled={page >= pages}
+              onClick={() => setPage((prev) => prev + 1)}
+            >
+              Next
+            </button>
+          }
+        </ul>
+      </div>
     </>
   );
 }
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,11 +1,112 @@
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
-import UsersList from "../components/UsersList";
+//import UsersList from "../components/UsersList";
+import { useState } from "react";
+import useFetch from "../useFetch";
+import "../pagination.css";
+import "../css/users.css";
+import { ErrorBoundary } from "react-error-boundary";
+//import {page, setPage} from "../components/UsersList"
+
+function UsersLists() {
+  const [page, setPage] = useState(1);
+  const { loading, error, data } = useFetch(
+    `https://randomuser.me/api/?results=40&seed=abc`
+  );
+  const PER_PAGE = 6;
+  const total = data?.results?.length;
+  //   const pages = 10;
+  const pages = Math.ceil(total / PER_PAGE);
+  //const skip = page * PER_PAGE - PER_PAGE;
+  if (loading) {
+    return <>Loading...</>;
+  }
+
+  if (!loading && error) {
+    return <>Error</>;
+  }
+
+  return (
+    <>
+      <h1>LIST OF USERS</h1>
+      <div className="box">
+        {/* // (page -1) * PER_PAGE, page * PER_PAGE    skip, skip + PER_PAGE*/}
+        {data?.results
+          .slice((page - 1) * PER_PAGE, page * PER_PAGE)
+          .map((each, index) => {
+            const name = `${each.name.title} ${each.name.first} ${each.name.last}`;
+            const image = `${each.picture.large}`;
+            const nationality = `${each.location.country}`;
+            return (
+              <div className="card">
+                <div className="imgBx">
+                  <img src={image} alt="images" />
+                </div>
+                <div className="details">
+                  <h2 key={name.toLowerCase().replaceAll(" ", "")}>
+                    {`${index + 1}.${name}`}
+                    <br />
+                    <span>{nationality}</span>
+                  </h2>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+
+      <div className="pagination-container">
+        <ul className="pagination">
+          {
+            <button
+              className="prev"
+              disabled={page <= 1}
+              onClick={() => setPage((prev) => prev - 1)}
+            >
+              Prev
+            </button>
+          }
+          {Array.from({ length: pages }, (_, index) => index + 1).map(
+            (each) => (
+              <li onClick={() => setPage(each)}>{each}</li>
+            )
+          )}
+          {
+            <button
+              className="next"
+              disabled={page >= pages}
+              onClick={() => setPage((prev) => prev + 1)}
+            >
+              Next
+            </button>
+          }
+        </ul>
+      </div>
+    </>
+  );
+}
+
 function Users() {
+  function ErrorFallback({ error, resetErrorBoundary }) {
+    return (
+      <div role="alert">
+        <p>Something went wrong:</p>
+        <pre style={{ color: "red" }}>{error.message}</pre>
+        <button onClick={resetErrorBoundary}>Try again</button>
+      </div>
+    );
+  }
+
   return (
     <>
       <Navigation />
-      <UsersList />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => UsersLists.setPage(1)}
+        // reset the state of your app so the error doesn't happen again
+        resetKeys={[UsersLists.page]}
+      >
+        <UsersLists />
+      </ErrorBoundary>
       <Footer />
     </>
   );
